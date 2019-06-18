b5e2da0b62a693a2200bcd7ba5aec0e848bb3c57
venoo123 <51963088+venoo123@users.noreply.github.com>
2019-06-18 20:03:40 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("590563909904236547")
 setInterval(function() {
-channel.send(`VEN  Ocrrets blhabl pls pls pls yes R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO Rsdasd3D veno r3dVENO R3D yesessdasdsdasdENO R3D veno r3d `);
+channel.send(`VEN  Ocrrets blhabl pls plr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dr3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3d3D veno r3dVENO R3D veno r3dVENO R3D veno r3dENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO Rsdasd3D veno r3dVENO R3D yesessdasdsdasdENO R3D veno r3d `);
 }, 30)
 })
 
