9225d7d0e0c6c4627dace92d7483c39a51cd33e3
venoo123 <51963088+venoo123@users.noreply.github.com>
2019-06-18 19:42:44 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("590563909904236547")
+setInterval(function() {
+channel.send(`VEN  Ocrrets blhabl pls pls pls yes R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO R3D veno r3dVENO Rsdasd3D veno r3dVENO R3D yesessdasdsdasdENO R3D veno r3d `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
